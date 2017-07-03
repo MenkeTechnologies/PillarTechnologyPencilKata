29599b3ebde03aa6783ccd3b07bd55a051bb1347
Jacob Menke <jamenk@email.wm.edu>
2017-07-03 18:47:59 -0400

    refactor erase method to move backwards through text

--- src/main/java/Pencil.java
+++ src/main/java/Pencil.java
@@ -1,5 +1,3 @@
-import java.util.stream.IntStream;
-
 /**
  * Created by jacobmenke on 7/2/17.
  */
@@ -11,6 +9,7 @@ class PointDurability {
 class PencilDefaults {
     public static final Integer DEFAULT_PENCIL_LENGTH = 100;
     public static final Integer DEFAULT_PENCIL_INITIAL_POINT_DURABILITY = 40_000;
+    public static final Integer DEFAULT_PENCIL_INITIAL_ERASER_DURABILITY = 40_000;
 }
 
 class SpecialCharacters {
@@ -20,29 +19,41 @@ class SpecialCharacters {
 public class Pencil {
     private Paper paper;
     private Integer pointDurability;
-    private Integer maximumPointDurability;
+    private Integer initialPointDurability;
     private Integer length;
+    private Integer eraserDurability;
+    private Integer initialEraserDurability;
 
     public Integer getLength() {
         return length;
     }
 
-    public Pencil(Integer length, Integer pointDurability) {
+    public Pencil(Integer length, Integer pointDurability, Integer eraserDurability) {
         this.length = length;
-        this.maximumPointDurability = pointDurability;
-        this.pointDurability = maximumPointDurability;
+        this.initialPointDurability = pointDurability;
+        this.pointDurability = initialPointDurability;
+        this.initialEraserDurability = eraserDurability;
+        this.eraserDurability = initialEraserDurability;
     }
 
     public Pencil(Integer length) {
-        this(length, PencilDefaults.DEFAULT_PENCIL_INITIAL_POINT_DURABILITY);
+        this(length, PencilDefaults.DEFAULT_PENCIL_INITIAL_POINT_DURABILITY, PencilDefaults.DEFAULT_PENCIL_INITIAL_ERASER_DURABILITY);
+    }
+
+    public Pencil(Integer length, Integer eraserDurability) {
+        this(length, PencilDefaults.DEFAULT_PENCIL_INITIAL_POINT_DURABILITY, eraserDurability);
     }
 
     public Integer getPointDurability() {
         return pointDurability;
     }
 
-    public Integer getMaximumPointDurability() {
-        return maximumPointDurability;
+    public Integer getInitialPointDurability() {
+        return initialPointDurability;
+    }
+
+    public Integer getEraserDurability() {
+        return eraserDurability;
     }
 
     public void setPaperToWriteTo(Paper paper) {
@@ -82,7 +93,7 @@ public class Pencil {
     public void sharpen() {
         length--;
         if (length > 0) {
-            pointDurability = maximumPointDurability;
+            pointDurability = initialPointDurability;
         }
     }
 
@@ -94,8 +105,17 @@ public class Pencil {
             Integer endingIndex = startindIndex + lengthToErase;
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder.append(paper.getContents().substring(0, startindIndex));
-            for (int i = startindIndex; i < endingIndex; i++) {
-                stringBuilder.append(SpecialCharacters.SPACE);
+            for (int i = endingIndex; i > startindIndex; i--) {
+                if (eraserDurability > 0) {
+                    stringBuilder.append(SpecialCharacters.SPACE);
+                    if (!Character.isWhitespace(paper.getContents().charAt(i))) {
+                        eraserDurability--;
+                    }
+                } else {
+                    System.err.println("My eraser is worn out. Buy a new pencil!");
+                    break;
+                }
+
             }
             stringBuilder.append(paper.getContents().substring(endingIndex));
             paper.eraseAndSetContents(stringBuilder.toString());
